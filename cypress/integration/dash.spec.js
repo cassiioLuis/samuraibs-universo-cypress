7448562c4fe386f905b08922a2c259c9fd9d6f01
Cassio Luis <ckr.luis@gmail.com>
2022-03-30 08:38:39 -0300

    finalizando novo modulo

---
+++ cypress/integration/dash.spec.js
@@ -0,0 +1,48 @@
+import loginPage from '../support/pages/login'
+import dashPage from '../support/pages/dash'
+
+describe('dashboard', function () {
+
+    context('quando o cliente faz um agendamento no app mobile', function () {
+
+        const data = {
+            customer: {
+                name: 'Nikki Sixx',
+                email: 'sixx@motleycrue.com',
+                password: 'pwd123',
+                is_provider: false,
+            },
+
+            provider: {
+                name: 'Ramon Valdes',
+                email: 'ramon@televisa.com',
+                password: 'pwd132',
+                is_provider: true
+            },
+
+            appointmentHour: '14:00'
+        }
+
+        before(function () {
+            cy.postUser(data.provider)
+            cy.postUser(data.customer)
+
+            cy.apiLogin(data.customer)
+
+            cy.setProviderId(data.provider.email)
+            cy.createAppointment(data.appointmentHour)
+        })
+
+        it('o mesmo deve ser exibido no dashboard', () => {
+
+            loginPage.go()
+            loginPage.form(data.provider)
+            loginPage.submit()
+
+            dashPage.calendarShouldBeVisible()
+            dashPage.selectDay(Cypress.env('appointmentDay'))
+            dashPage.appointmentShouldBeVisible(data.customer, data.appointmentHour)
+        })
+
+    })
+})
